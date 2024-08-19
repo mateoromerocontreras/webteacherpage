de7fb06adcb9faf846046ec5fc5d81b2bc656e94
mateoromero <mateo.romero.contreras@gmail.com>
2024-08-19 12:02:19 -0300

    Actividad 1 SO

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
 console.log("¡Bienvenido a Mi Página Web!");
+// main.js
+document.getElementById('entrada-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const titulo = document.getElementById('titulo').value;
+    const contenido = document.getElementById('contenido').value;
+
+    const nuevaEntrada = document.createElement('div');
+    nuevaEntrada.classList.add('nueva-entrada');
+
+    const entradaTitulo = document.createElement('h3');
+    entradaTitulo.textContent = titulo;
+
+    const entradaContenido = document.createElement('p');
+    entradaContenido.textContent = contenido;
+
+    nuevaEntrada.appendChild(entradaTitulo);
+    nuevaEntrada.appendChild(entradaContenido);
+
+    document.getElementById('entradas').appendChild(nuevaEntrada);
+
+    document.getElementById('entrada-form').reset();
+});
